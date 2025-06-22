f4046047c501b47baa2efaede7d1e0056d8451a8
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-06-22 12:02:32 +0800

    获取主活动的网卡的IPV4地址

---
+++ MulticastLocalMessage/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastLocalMessage
+{
+    public class Utils
+    {
+        /// <summary>
+        /// 获取主活动网卡的IPv4地址
+        /// </summary>
+        /// <returns></returns>
+        public static IPAddress GetPrimaryIPv4Address()
+        {
+            var activeInterface = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(ni => ni.OperationalStatus == OperationalStatus.Up &&
+                                    ni.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                                    !ni.Description.Contains("Virtual") &&
+                                    !ni.Description.Contains("Pseudo") &&
+                                    !ni.Name.StartsWith("vEthernet"));
+
+            if (activeInterface != null)
+            {
+                var ip = activeInterface.GetIPProperties().UnicastAddresses
+                    .FirstOrDefault(addr => addr.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+
+                return ip?.Address;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ NUnitTest/UnitTestUtils.cs
@@ -0,0 +1,18 @@
+﻿using MulticastLocalMessage;
+
+namespace NUnitTest;
+
+public class UnitTestUtils
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GetPrimaryIPv4Address()
+    {
+        var q = Utils.GetPrimaryIPv4Address();
+        Assert.Pass();
+    }
+}
