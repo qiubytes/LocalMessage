7342a129af1a119f4f615a4c2fb370c2b8c0e7ec
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2025-06-22 13:01:52 +0800

    测试单播udp消息

--- NUnitTest/UnitTestUtils.cs
+++ NUnitTest/UnitTestUtils.cs
@@ -1,4 +1,5 @@
 ﻿using MulticastLocalMessage;
+using System.Net.Sockets;
 
 namespace NUnitTest;
 
@@ -13,6 +14,10 @@ public class UnitTestUtils
     public void GetPrimaryIPv4Address()
     {
         var q = Utils.GetPrimaryIPv4Address();
+        UdpClient udpClient = new UdpClient();
+        udpClient.Connect(q, 5000);//不能是127.0.0.1
+        byte[] bytes = System.Text.Encoding.UTF8.GetBytes("aaa");
+        udpClient.Send(bytes);
         Assert.Pass();
     }
 }
